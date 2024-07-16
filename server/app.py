1a69e7c5507751960bd2c8c1b7e267b709729005
Tulley Mwenda <Tulleyteyez@gmail.com>
2024-07-16 17:59:34 +0300

    add models and app.py

--- server/app.py
+++ server/app.py
@@ -1,23 +1,30 @@
-#!/usr/bin/env python3
+from flask import Flask
+from flask_migrate import Migrate
+from flask_restful import Api
+from models import db, User, Movie, Review, Genre, MovieGenre
+from watcher import watcher_bp
+import os
 
-# Standard library imports
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
 
-# Remote library imports
-from flask import request
-from flask_restful import Resource
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SECRET_KEY'] = 'You will never walk Alone'
+app.config['UPLOAD_FOLDER'] = 'uploads/'
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.json.compact = False
 
-# Local imports
-from config import app, db, api
-# Add your model imports
+db.init_app(app)
+migrate = Migrate(app, db)
 
+# Register the blueprint
+app.register_blueprint(watcher_bp, url_prefix='/api')
 
-# Views go here!
-
-@app.route('/')
+@app.route("/")
 def index():
-    return '<h1>Project Server</h1>'
+    return "<h1>PROJECT</h1>"
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
-
